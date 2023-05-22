441ee14ebe28af8c8d1a9a32d1a89081fb998241
Edwar421 <134245123+Edwar421@users.noreply.github.com>
2023-05-22 10:24:51 -0500

    Add files via upload

---
+++ src/Bridge/BridgePrueba.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bridge;
+
+import Abstracta.*;
+import Bridge.Output;
+import implementador.*;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class BridgePrueba {
+ 
+    public static void main(String[] args) {
+        Servicio servicio = new ServicioCortePelo();
+        Output objSalida = new Output();
+        Animal perro = new Perro(servicio);
+        Animal gato = new Gato(servicio);
+        
+        objSalida.show("Cortes de Pelo");
+        objSalida.show(perro.sano());
+        objSalida.show(perro.enfermo());
+        objSalida.show(gato.sano());
+        objSalida.show(gato.enfermo());
+        
+        Servicio servicio2 = new ServicioBano();
+        perro.setServicio(servicio2);
+        gato.setServicio(servicio2);
+        
+        objSalida.show("\nBano");
+        objSalida.show(perro.sano());
+        objSalida.show(perro.enfermo());
+        objSalida.show(gato.sano());
+        objSalida.show(gato.enfermo());
+        
+    }   
+}
---
+++ src/implementador/ServicioCortePelo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementador;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ServicioCortePelo extends Servicio{
+
+    @Override
+    public String realizarServicio() {
+        return "Se le esta realizando un corte de pelo.";
+    }
+
+    @Override
+    public String noRealizarServicio() {
+        return "No se le puede realizar el corte, puede empeorar";
+    }
+}
